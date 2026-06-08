489f7a55a20173b1ddc9586d4e065c01c4fefcbb
TAREMWA-BENJAMIN <taremwabenja.jb@gmail.com>
2026-06-08 16:31:19 +0300

    modification of the UI

--- magezi-ga-lawyer/src/App.tsx
+++ magezi-ga-lawyer/src/App.tsx
@@ -1,23 +1,18 @@
 import './App.css'
-import { CaseProvider } from './contexts/CaseContext'
 import { LanguageProvider } from './contexts/LanguageContext'
 import Navigation from './components/Navigation'
 import HeroSection from './components/HeroSection'
 import LegalLibrary from './components/LegalLibrary'
-import TemplateFormBuilder from './components/TemplateFormBuilder'
-import CaseTracker from './components/CaseTracker'
 
 function App() {
   return (
     <LanguageProvider>
-      <CaseProvider>
-        <div className="app-shell">
+      <div className="app-shell">
           <Navigation />
           <main>
             <HeroSection />
             <LegalLibrary />
-            <TemplateFormBuilder />
-            <CaseTracker />
+            {/* <CaseTracker /> */}
             <section className="contact-section" id="contact">
               <div>
                 <p className="eyebrow">Support with confidence</p>
@@ -30,7 +25,7 @@ function App() {
               </div>
               <div className="contact-card">
                 <p>Emergency support line</p>
-                <strong>+256 700 123 456</strong>
+                <strong>+256 791862269</strong>
                 <a className="hero-button" href="tel:+256700123456">
                   Call now
                 </a>
@@ -38,7 +33,6 @@ function App() {
             </section>
           </main>
         </div>
-      </CaseProvider>
     </LanguageProvider>
   )
 }
